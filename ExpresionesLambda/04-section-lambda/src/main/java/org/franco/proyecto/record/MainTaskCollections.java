6cca916ad2c929fc5849f27f36311de1bb226dbd
Jonathan Franco <jcrfranco7@gmail.com>
2025-10-12 11:36:00 -0500

    cambio de nombre

--- ExpresionesLambda/04-section-lambda/src/main/java/org/franco/proyecto/record/MainTaskCollections.java
+++ ExpresionesLambda/04-section-lambda/src/main/java/org/franco/proyecto/record/MainTaskCollections.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class MainTask {
+public class MainTaskCollections {
 
     static void main(String[] args) {
         List<ProductDto> products = List.of(
